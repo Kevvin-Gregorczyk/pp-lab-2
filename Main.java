7ab5d1e15738f3e81a308c675f97cda603c99ef9
unknown <kewinpriv@gmail.com>
2024-04-06 12:40:41 +0200

    Zad3

--- Main.java
+++ Main.java
@@ -7,12 +7,23 @@ public class Main {
         System.out.println("Podaj liczbę przedmiotów:");
         int numOfSubjects = scanner.nextInt();
 
+        double[] grades = new double[numOfSubjects];
+
+        for (int i = 0; i < numOfSubjects; i++) {
+            System.out.println("Podaj ocenę z przedmiotu " + (i + 1) + ":");
+            grades[i] = scanner.nextDouble();
+        }
+
+        System.out.println("Oceny z przedmiotów:");
+        for (int i = 0; i < numOfSubjects; i++) {
+            System.out.println("Ocena z przedmiotu " + (i + 1) + ": " + grades[i]);
+        }
+
         double sum = 0;
-        for (int i = 1; i <= numOfSubjects; i++) {
-            System.out.println("Podaj ocenę z przedmiotu " + i + ":");
-            double grade = scanner.nextDouble();
+        for (double grade : grades) {
             sum += grade;
         }
+        System.out.println("Suma ocen: " + sum);
 
         double average = sum / numOfSubjects;
         System.out.println("Średnia ocen z przedmiotów: " + average);
